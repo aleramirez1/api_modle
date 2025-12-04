c01d1fa1099196b5688cc65891d5a367b7103dd0
Alexia Vanelli <aleramirez@example.com>
2025-12-03 20:27:48 -0600

    falta

--- services/grupos.service.js
+++ services/grupos.service.js
@@ -3,10 +3,13 @@ const db = require('../config/db');
 module.exports = {
   getAll: async () => {
     const [rows] = await db.query(`
-      SELECT g.id, g.nombre, a.nombre AS asignatura, d.nombre AS docente
-      FROM grupos g
-      LEFT JOIN asignaturas a ON g.asignatura_id = a.id
-      LEFT JOIN docentes d ON g.docente_id = d.id
+      SELECT 
+        id_grupo,
+        nombre_grupo AS nombre,
+        nombre_de_asignatura AS asignatura,
+        nombre_docente AS docente,
+        alumnos
+      FROM grupos
     `);
     return rows;
   }
